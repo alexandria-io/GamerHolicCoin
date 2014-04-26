274e83ef424093b159663bbe7dba5024606cb74f
CryptoGir <josh.eaker@gmail.com>
2014-04-26 15:07:58 -0400

    Revert "comma"
    This reverts commit d181f682f53af249f618c8503b48ae4a054ec5fb.

--- src/main.cpp
+++ src/main.cpp
@@ -1593,7 +1593,7 @@ bool CBlock::ConnectBlock(CTxDB& txdb, CBlockIndex* pindex, bool fJustCheck)
 
     if (IsProofOfWork())
     {
-        int64_t nReward = GetProofOfWorkReward(nFees, pindex->nHeight);
+        int64_t nReward = GetProofOfWorkReward(nFees pindex->nHeight);
         // Check coinbase reward
         if (vtx[0].GetValueOut() > nReward)
             return DoS(50, error("ConnectBlock() : coinbase reward exceeded (actual=%"PRId64" vs calculated=%"PRId64")",
