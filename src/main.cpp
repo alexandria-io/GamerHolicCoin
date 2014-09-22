e4e7b5b07c2875bb6f94ec1ed828c4112468e940
Gamerholic Dev <gamerholicdev@gamerholic.com>
2014-09-21 17:07:29 -1000

    Modify pchMessageStart values

--- src/main.cpp
+++ src/main.cpp
@@ -2481,10 +2481,10 @@ bool LoadBlockIndex(bool fAllowNew)
 
     if (fTestNet)
     {
-        pchMessageStart[0] = 0xf2;
-        pchMessageStart[1] = 0xc0;
-        pchMessageStart[2] = 0xef;
-        pchMessageStart[3] = 0xcd;
+        pchMessageStart[0] = 0xf4;
+        pchMessageStart[1] = 0xc2;
+        pchMessageStart[2] = 0xf1;
+        pchMessageStart[3] = 0xcf;
 
         bnTrustedModulus.SetHex("f0d14cf72623dacfe738d0892b599be0f31052239cddd95a3f25101c801dc990453b38c9434efe3f372db39a32c2bb44cbaea72d62c8931fa785b0ec44531308df3e46069be5573e49bb29f4d479bfc3d162f57a5965db03810be7636da265bfced9c01a6b0296c77910ebdc8016f70174f0f18a57b3b971ac43a934c6aedbc5c866764a3622b5b7e3f9832b8b3f133c849dbcc0396588abcd1e41048555746e4823fb8aba5b3d23692c6857fccce733d6bb6ec1d5ea0afafecea14a0f6f798b6b27f77dc989c557795cc39a0940ef6bb29a7fc84135193a55bcfc2f01dd73efad1b69f45a55198bd0e6bef4d338e452f6a420f1ae2b1167b923f76633ab6e55");
         bnProofOfWorkLimit = bnProofOfWorkLimitTestNet; // 16 bits PoW target limit for testnet
@@ -2841,7 +2841,7 @@ bool static AlreadyHave(CTxDB& txdb, const CInv& inv)
 // The message start string is designed to be unlikely to occur in normal data.
 // The characters are rarely used upper ASCII, not valid as UTF-8, and produce
 // a large 4-byte int at any alignment.
-unsigned char pchMessageStart[4] = { 0x69, 0x42, 0x05, 0x22 };
+unsigned char pchMessageStart[4] = { 0x6b, 0x44, 0xb6, 0xdb };
 
 bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv)
 {
